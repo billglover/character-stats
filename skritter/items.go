d43e006ec4ced26d1c0a15cb26bcd59b0e35aa9d
Bill Glover <bglover@vmware.com>
2021-10-02 22:07:39 +0100

    Retrieve all Items and Vocab from /items endpoint. Fixes #1
    * Ignore binary and log files
    * Retrieve all items from the /Items endpoint
    * Include Heisig and Vocab entries
    * Parse Skritter response and print vocab

---
+++ skritter/items.go
@@ -0,0 +1,144 @@
+package skritter
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func (c *Client) Items() error {
+
+	vocab := map[string]string{}
+	items := 0
+
+	path := c.BaseURL.String() + "/items"
+	cursor := ""
+
+	for {
+
+		req, err := http.NewRequest(http.MethodPost, path, nil)
+		if err != nil {
+			return err
+		}
+
+		q := req.URL.Query()
+		q.Add("include_vocabs", "true")
+		q.Add("include_heisigs", "true")
+		if cursor != "" {
+			q.Add("cursor", cursor)
+		}
+		req.URL.RawQuery = q.Encode()
+
+		req.Header.Add("Authorization", "Bearer "+c.token)
+		req.Method = http.MethodGet
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		bodyString := string(bodyBytes)
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("%s: %s", resp.Status, bodyString)
+		}
+
+		type Response struct {
+			Cursor     string  `json:"cursor"`
+			Items      []Item  `json:"Items"`
+			Vocabs     []Vocab `json:"Vocabs"`
+			StatusCode int     `json:"statusCode"`
+		}
+
+		var r Response
+		err = json.Unmarshal(bodyBytes, &r)
+		if err != nil {
+			return err
+		}
+
+		cursor = r.Cursor
+		items += len(r.Items)
+
+		for _, v := range r.Vocabs {
+			en := v.Definitions["en"]
+			vocab[v.Writing] = strings.ReplaceAll(en, "\n", "\\n")
+		}
+
+		if cursor == "" {
+			break
+		}
+
+		//time.Sleep(5 * time.Millisecond)
+	}
+
+	fmt.Println("Items retrieved:", items)
+	fmt.Println("Vocab retrieved:", len(vocab))
+	for zn, en := range vocab {
+		fmt.Println(zn, en)
+	}
+
+	return nil
+}
+
+// Item is the atomic unit of learning in Skritter. It is used to track
+// reviews and learning. An item could be the writing for a character, or
+// the tone of a word.
+// Source Documentation: https://www.skritter.com/api/v0/docs/entities/items
+type Item struct {
+	Lang             string   `json:"lang"`
+	Style            string   `json:"style"`
+	Interval         int      `json:"interval"`
+	Last             int      `json:"last"`
+	Created          int      `json:"created"`
+	Successes        int      `json:"successes"`
+	TimeStudied      int      `json:"timeStudied"`
+	Changed          int      `json:"changed"`
+	Next             int      `json:"next"`
+	Reviews          int      `json:"reviews"`
+	PreviousInterval int      `json:"previousInterval,omitempty"`
+	SectionIds       []string `json:"sectionIds"`
+	VocabListIds     []string `json:"vocabListIds"`
+	VocabIds         []string `json:"vocabIds"`
+	Part             string   `json:"part"`
+	PreviousSuccess  bool     `json:"previousSuccess"`
+	ID               string   `json:"id"`
+}
+
+// Vocab is complementary to the Item entity. It and provides all the user-specific
+// settings for a word, as well as all the information about the word.
+// Source Documentation: https://www.skritter.com/api/v0/docs/entities/vocabs
+type Vocab struct {
+	Lang             string            `json:"lang"`
+	Priority         int               `json:"priority"`
+	Style            string            `json:"style"`
+	Audio            string            `json:"audio"`
+	Toughness        int               `json:"toughness"`
+	DictionaryLinks  map[string]string `json:"dictionaryLinks"`
+	HeisigDefinition string            `json:"heisigDefinition"`
+	Created          int               `json:"created,omitempty"`
+	Ilk              string            `json:"ilk"`
+	Writing          string            `json:"writing"`
+	Audios           []Audio           `json:"audios"`
+	AudioURL         string            `json:"audioURL"`
+	ToughnessString  string            `json:"toughnessString"`
+	Definitions      map[string]string `json:"definitions"`
+	//	Starred          bool              `json:"starred"`
+	Reading string `json:"reading"`
+	ID      string `json:"id"`
+}
+
+// Audio contains links to the pronounciation of a vocab.
+type Audio struct {
+	Source  string `json:"source"`
+	Reading string `json:"reading"`
+	ID      string `json:"id"`
+	Writing string `json:"writing"`
+	Mp3     string `json:"mp3"`
+}
